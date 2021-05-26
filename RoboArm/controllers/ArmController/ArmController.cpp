bf5dbde0b23b0e0c9aa7aa6778fbd3bc11bb8fb5
Miel <MielFidder@gmail.com>
2021-05-26 09:42:59 +0200

    Robot Arm

---
+++ RoboArm/controllers/ArmController/ArmController.cpp
@@ -0,0 +1,81 @@
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/Joystick.hpp>
+
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+#define TIME_STEP 64
+#define SPEEDMULTARM 15000
+#define SPEEDMULTGRIP 35000
+
+int main(int argc, char **argv) {
+  // create the Robot instance.
+  Robot *robot = new Robot();  
+  Motor *motor[8];
+  
+  Joystick joystick;
+  joystick.enable(TIME_STEP);
+  
+  char wheelMotors[8][8] = {"1", "2", "3", "4", "5", "6", "7", "7 left"};
+  for(int i = 0; i < 8; i++){
+    motor[i] = robot->getMotor(wheelMotors[i]);
+    motor[i]->setVelocity(0.0);
+    motor[i]->setPosition(INFINITY);
+  }
+  
+  motor[6]->setPosition(INFINITY);
+  
+
+    
+    
+  while (robot->step(TIME_STEP) != -1) {
+    // 1 & 2 moeten blijven staan
+    // 7 & 7 left zijn grijper
+    // 3 & 5 voor en achter
+    // 4 & 6 rotaties
+    //alles -1 voor index
+    
+    //velocity
+    double stickX = joystick.getAxisValue(1);
+    double stickY = joystick.getAxisValue(0);
+    
+    double stick2X = joystick.getAxisValue(2);
+    double stick2Y = joystick.getAxisValue(3);
+    
+    double stickYpower = stickY / SPEEDMULTARM;
+    double stickXpower = 0 - (stickX / SPEEDMULTARM);
+    
+    double stick2Ypower = stick2Y / SPEEDMULTGRIP;
+    double stick2Xpower = 0 - (stick2X / SPEEDMULTGRIP);
+        
+
+    //std::cout<< "y: "<< stick2Ypower <<std::endl;
+    //std::cout<< "x: "<< stick2Xpower <<std::endl;
+    
+    std::cout<< "x: "<< motor[6]->getTorqueFeedback() <<std::endl;
+   
+    
+    if(stickYpower < 1.2 && stickYpower > -1.2)
+      motor[4]->setVelocity(0.0);
+    else
+      motor[4]->setVelocity(stickYpower);
+    
+    if(stickXpower < 1.2 && stickXpower > -1.2)
+      motor[3]->setVelocity(0.0);
+    else
+      motor[3]->setVelocity(stickXpower);
+      
+    if(stick2Ypower < 0.3 && stick2Ypower > -0.3){
+        motor[6]->setVelocity(0.0);
+        motor[7]->setVelocity(0.0);
+     }else{
+        motor[6]->setVelocity(stick2Ypower);
+        motor[7]->setVelocity(stick2Ypower);
+     }
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
--- WeBots/controllers/RupsbandController/RupsbandController.cpp
+++ WeBots/controllers/RupsbandController/RupsbandController.cpp
@@ -1,18 +1,11 @@
 #include <webots/Robot.hpp>
-#include <webots/Keyboard.hpp>
 #include <webots/Joystick.hpp>
 
 // Added a new include file
 #include <webots/Motor.hpp>
 
 #define TIME_STEP 64
-#define SPEED 6
-//define key codes
-#define UP 315
-#define DOWN 317
-#define LEFT 314
-#define RIGHT 316
-
+#define SPEEDMULT 1500
 
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
@@ -22,11 +15,8 @@ bool Array[4];
 int main(int argc, char **argv) {
  Robot *robot = new Robot();
 
-Keyboard keyboard;
 Joystick joystick;
-
 joystick.enable(TIME_STEP);
-keyboard.enable(TIME_STEP);
 
 Motor *motor[10];
 
@@ -34,7 +24,7 @@ char wheelMotors[10][16] = {"wheel_motor00","wheel_motor01","wheel_motor02",
   "wheel_motor03","wheel_motor04","wheel_motor05","wheel_motor06",
   "wheel_motor07","wheel_motor08","wheel_motor09"};
 for(int i = 0; i < 10; i++){
-  motor[i] =  robot->getMotor(wheelMotors[i]);
+  motor[i] = robot->getMotor(wheelMotors[i]);
   motor[i]->setVelocity(0.0);
 }
 
@@ -44,14 +34,11 @@ for(int i = 0; i < 10; i++){
 }
 
  while (robot->step(TIME_STEP) != -1){
-   int key = keyboard.getKey();
    int stickX = joystick.getAxisValue(1);
    int stickY = joystick.getAxisValue(0);
-
-   int speedMult = 1500;
   
-   int stickYinv = (0-stickY)/speedMult;
-   int stickXinv = (0-stickX)/speedMult;
+   int stickYinv = (0-stickY)/SPEEDMULT;
+   int stickXinv = (0-stickX)/SPEEDMULT;
 
    int v = (100 - abs(stickXinv)) * (stickYinv/100) + stickYinv;
    int w = (100 - abs(stickYinv)) * (stickXinv/100) + stickXinv;
